56f13d55bd47e4c2bd64f8289d45c12d2d1f3342
Dorthe Luebbert <luebbert@rewe-test>
2019-05-14 01:51:16 -0400

    Code aufgeräumt. Jetzt drei Eier statt zwei.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 # /path/to/project/main.py
 def eier_in_pfanne_aufschlagen_und_gewuerze_und_braten():
-    print("Eier in einer Pfanne aufschlagen, Salz und Pfeffer hinzu und braten.")
+    print("Zwei Eier in einer Pfanne aufschlagen, Salz und Pfeffer hinzu und braten.")
+
+eier_in_pfanne_aufschlagen_und_gewuerze_und_braten()
